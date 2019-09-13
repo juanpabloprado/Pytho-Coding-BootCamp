7fc28bbe97aa5842b3253119f20c6f536ff0a2a0
Juan Prado <juan@juanpabloprado.com>
2019-09-12 19:49:05 -0500

    rm ipynb_checkpoints

--- 01_Day1/03-Activities/.ipynb_checkpoints/07-0-checkpoint.py
+++
@@ -1,7 +0,0 @@
-first_name = input("What is your first name?")
-print("Hello,", first_name)
-if first_name == "Juan":
-    print(first_name, "is learning Python")
-else:
-    print("You should totally learn Python, {}!".format(first_name))
-print("Have a great day {}!".format(first_name))
